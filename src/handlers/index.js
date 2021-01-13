4147ee38d1ca592ff4246d5b7ab0f676e122e17c
Kevin Leyow <kleyow@gmail.com>
2021-01-13 06:45:37 -0500

    chore: re-enable license scan and add licenses to files

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -1,3 +1,12 @@
+/**************************************************************************
+ *  (C) Copyright Mojaloop Foundation 2020 - All rights reserved.         *
+ *                                                                        *
+ *  This file is made available under the terms of the license agreement  *
+ *  specified in the corresponding source code repository.                *
+ *                                                                        *
+ *  Original Author:                                                      *
+ *       Kevin Leyow - kevin.leyow@modusbox.com                           *
+ **************************************************************************/
 'use strict'
 
 const OpenapiBackend = require('@mojaloop/central-services-shared').Util.OpenapiBackend
--- src/lib/logger-plugin.js
+++ src/lib/logger-plugin.js
@@ -1,29 +1,17 @@
-/*****
- License
- --------------
- Copyright © 2017 Bill & Melinda Gates Foundation
- The Mojaloop files are made available by the Bill & Melinda Gates Foundation under the Apache License, Version 2.0 (the "License") and you may not use these files except in compliance with the License. You may obtain a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
- Contributors
- --------------
- This is the official list of the Mojaloop project contributors for this file.
- Names of the original copyright holders (individuals or organizations)
- should be listed with a '*' in the first column. People who have
- contributed from an organization can be listed under the organization
- that actually holds the copyright for their contributions (see the
- Gates Foundation organization for an example). Those individuals should have
- their names indented and be marked with a '-'. Email address can be added
- optionally within square brackets <email>.
- * Gates Foundation
- * Georgi Georgiev <georgi.georgiev@modusbox.com>
- * Valentin Genev <valentin.genev@modusbox.com>
- * Deon Botha <deon.botha@modusbox.com>
- * Rajiv Mothilal <rajiv.mothilal@modusbox.com>
- * Miguel de Barros <miguel.debarros@modusbox.com>
-
- --------------
- ******/
+/**************************************************************************
+ *  (C) Copyright Mojaloop Foundation 2020 - All rights reserved.         *
+ *                                                                        *
+ *  This file is made available under the terms of the license agreement  *
+ *  specified in the corresponding source code repository.                *
+ *                                                                        *
+ *  Original Author:                                                      *
+ *       Valentin Genev - valentin.genev@modusbox.com                     *
+ *  Contributors:                                                         *
+ *       Georgi Georgiev <georgi.georgiev@modusbox.com>                   *
+ *       Deon Botha <deon.botha@modusbox.com>                             *
+ *       Rajiv Mothilal <rajiv.mothilal@modusbox.com>                     *
+ *       Miguel de Barros <miguel.debarros@modusbox.com>                  *
+ **************************************************************************/
 
 const Logger = require('@mojaloop/central-services-logger')
 const checkEmpty = require('./truthyProperty')
